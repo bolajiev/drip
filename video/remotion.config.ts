e110b9e2c1ee8b6dcbc91a3ac7abcc8cb580b416
drip <drip@hackathon.dev>
2026-08-11 15:56:18 +0200

    repo: remove video/ from repo (keep local)

--- video/remotion.config.ts
+++
@@ -1,5 +0,0 @@
-import { Config } from "@remotion/cli/config";
-
-Config.setVideoImageFormat("jpeg");
-Config.setOverwriteOutput(true);
-Config.setBrowserExecutable("/root/.cache/ms-playwright/chromium-1234/chrome-linux64/chrome");
